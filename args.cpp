8352e50b53bef74eebdc7ac8c31db322c32bccac
Arshaq <arshahm2004@gmail.com>
2024-02-16 18:42:26 +0000

    args

--- args.cpp
+++ args.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
-
 using namespace std;
 
-int main() {
-    
-
+int main(int argc, char* argv[]) {
+    cout << "Program name: '" << argv[0] << "'" << endl;
+    cout << "called with " << argc - 1 << " arguments: ";
+    for (int i = 1; i < argc; ++i) {
+        cout << "'" << argv[i] << "'";
+        if (i < argc - 1) {
+            cout << " ";
+        }
+    }
+    cout << endl;
     return 0;
 }
